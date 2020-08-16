cfaf58cb8f69022fae3e733e8490a05541d52926
geoirb <morochev.g@gmail.com>
2020-08-17 00:19:20 +0300

    del \n

--- build/init.sql
+++ build/init.sql
@@ -2,6 +2,5 @@ CREATE TABLE public.news (
 	id serial NOT NULL,
 	title varchar NOT NULL,
 	pubDate timestamp NOT NULL,
-	CONSTRAINT news_pkey PRIMARY KEY (id)
-
+	CONSTRAINT news_pkey PRIMARY KEY 
 );
